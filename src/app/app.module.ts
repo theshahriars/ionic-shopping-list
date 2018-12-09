27cc54fd78e69f88caf531ad93d50e7e047975b0
Shahriar Shabbir <theshahriars@gmail.com>
2018-12-10 00:18:19 +0600

    Firebase config & New page added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import {ErrorHandler, NgModule} from '@angular/core';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {FIREBASE_CONFIG} from "./firebase.config";
 
 import {MyApp} from './app.component';
 
@@ -12,7 +15,9 @@ import {MyApp} from './app.component';
     ],
     imports: [
         BrowserModule,
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(FIREBASE_CONFIG),
+        AngularFireDatabaseModule
     ],
     bootstrap: [IonicApp],
     entryComponents: [
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBINseri__Q2EGRD9E02VLFhDf9U8YZp8c",
+    authDomain: "ionic-shopping-list-8eec0.firebaseapp.com",
+    databaseURL: "https://ionic-shopping-list-8eec0.firebaseio.com",
+    projectId: "ionic-shopping-list-8eec0",
+    storageBucket: "ionic-shopping-list-8eec0.appspot.com",
+    messagingSenderId: "561774608845"
+};
