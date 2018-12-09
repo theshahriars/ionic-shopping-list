06347a2f2d694aa643542f82c487d813b07b19cd
Shahriar Shabbir <theshahriars@gmail.com>
2018-12-10 01:08:41 +0600

    Add Form Page Added

---
+++ src/models/item/item.model.ts
@@ -0,0 +1,6 @@
+export interface Item {
+    key?: string;
+    name: string;
+    price: string;
+    quantity: string;
+}
--- src/pages/add-item/add-item.ts
+++ src/pages/add-item/add-item.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddItemPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Item} from "../../models/item/item.model";
 
 @IonicPage()
 @Component({
-  selector: 'page-add-item',
-  templateUrl: 'add-item.html',
+    selector: 'page-add-item',
+    templateUrl: 'add-item.html',
 })
 export class AddItemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    item: Item = {
+        name: '',
+        price: '',
+        quantity: ''
+    };
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddItemPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad AddItemPage');
+    }
 
 }
